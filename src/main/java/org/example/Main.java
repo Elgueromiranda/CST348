c8d20f432eccda0a00beda412aa4d05017347734
Collin-Fox <131672263+Collin-Fox@users.noreply.github.com>
2023-11-30 13:24:14 -0600

    huifgrvd

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Die die_one = new Die();
+        Die die_two = new Die();
+        Die die_three = new Die();
+        Die die_four = new Die();
+        Die die_five = new Die();
+        Die die_six = new Die();
+
+        String input = JOptionPane.showInputDialog("Would you like to roll the six die? Enter y to reroll:\n");
+
+
+    }
+}
