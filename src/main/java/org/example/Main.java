42dfdb2838e69518f9b522143e6549e0d837552e
cardGame <elgueromiranda@protonmail.com>
2023-12-02 13:25:56 -0600

    added user UI

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,21 @@ public class Main {
 
         String input = JOptionPane.showInputDialog("Would you like to roll the six die? Enter y to reroll:\n");
 
+        
+
+       if (input.equals("y")) {
+           die_one.roll();
+           die_two.roll();
+           die_three.roll();
+           die_four.roll();
+           die_five.roll();
+           die_six.roll();
+           System.out.println(die_one.value + " " + die_two.value + " " + die_three.value + " " + die_four.value + " " + die_five.value + " " + die_six.value);
+
+       } else {
+           System.out.println("Goodbye");
+       }
+
 
     }
 }
